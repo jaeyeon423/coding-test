977d27e75d6e9c76c59a7ad2ea5a6f30d0006fce
jaeyeonkim <jaeyeon423@gmail.com>
2023-01-24 23:31:14 +0900

    시저암호 다시 풀기

--- level1/Test15.java
+++ level1/Test15.java
@@ -1,6 +1,22 @@
 public class Test15 {
     public static void main(String[] args) {
-        char a = "a";
-        System.out.println(a+1);
+        String s = "az";
+        int n = 4;
+
+        String answer = " ";
+
+        char[] char_arr = s.toCharArray();
+        for(char c : char_arr){
+            if(c == " ".charAt(0)){
+                answer += " ";
+            }else if(c <= 'Z'){
+                int tmp = c + n;
+                if(tmp > 'Z'){
+                    tmp -= 'Z';
+                }
+            }else{
+
+            }
+        }
     }
 }
